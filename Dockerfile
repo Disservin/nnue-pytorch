298c0e2db9e20aab6f56e0afdecd34110356a303
Disservin <disservin.social@gmail.com>
2025-06-09 13:57:33 +0200

    add compiling data loader

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM nvcr.io/nvidia/pytorch:22.12-py3
 
+USER root
+
 RUN apt-get update && apt-get install -y \
     git \
     curl \
--- setup_script.sh
+++ setup_script.sh
@@ -5,8 +5,10 @@ if [ ! -d ".venv" ]; then
     source .venv/bin/activate
     # pip --no-cache-dir install torch --index-url https://download.pytorch.org/whl/cu118
     pip install -r requirements.txt
+    python3 -m pip install --upgrade pip
 fi
 
 echo "source /workspace/nnue-pytorch/.venv/bin/activate" >> /root/.bashrc
+sh compile_data_loader.bat
 
 exec "$@"
