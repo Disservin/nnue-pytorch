6de8c8553db0340e462984a727b238a09d183969
Disservin <disservin.social@gmail.com>
2025-06-11 21:01:48 +0200

    Update pytorch lightning to 2

--- run_docker.sh
+++ run_docker.sh
@@ -8,22 +8,22 @@ read DATA_PATH
 DATA_PATH=${DATA_PATH}
 echo "Using data path: $DATA_PATH"
 
-if [ "$(docker ps -aq -f name=nnue-container)" ]; then
-  if [ ! "$(docker ps -q -f name=nnue-container)" ]; then
-    echo "Starting existing container 'nnue-container'..."
-    docker start -i nnue-container
-  else
-    echo "Container 'nnue-container' is already running. Attaching..."
-    docker attach nnue-container
-  fi
-else
+# if [ "$(docker ps -aq -f name=nnue-container)" ]; then
+#   if [ ! "$(docker ps -q -f name=nnue-container)" ]; then
+#     echo "Starting existing container 'nnue-container'..."
+#     docker start -i nnue-container
+#   else
+#     echo "Container 'nnue-container' is already running. Attaching..."
+#     docker attach nnue-container
+#   fi
+# else
   echo "Creating new container 'nnue-container'..."
-  docker run -it --name nnue-container \
+  docker run -it \
     --gpus all \
     -v "$(pwd)":/workspace/nnue-pytorch \
-    -v "$DATA_PATH":/workspace/data \
+    -v "$DATA_PATH":/data \
     --ipc=host \
     --ulimit memlock=-1 \
     --ulimit stack=67108864 \
     nnue-pytorch
-fi
+# fi
